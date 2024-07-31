11fcec2c4e2c36248b5e69106f37cf360c371659
Gabi <gabinelepcu29@gmail.com>
2024-07-31 13:58:11 +0300

    update

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -16,7 +16,7 @@ const useSignup = () => {
         try {
             setError(null);
             setLoading(true);
-            const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/signup`, {
+            const res = await fetch(`165.22.95.36/api/auth/signup`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
