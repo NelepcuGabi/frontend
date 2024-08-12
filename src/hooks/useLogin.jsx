49cf686d21571c39a9da8b358bf7fa8b629fe5a9
Gabi <gabinelepcu29@gmail.com>
2024-08-12 14:16:02 +0300

    update

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -13,7 +13,7 @@ const useLogin = () => {
             
             setError(null);
             setLoading(true);
-            const res = await fetch(`https://telacad-app-fyzcu.ondigitalocean.app/api/auth/login`, {
+            const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/login`, {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json',
