1546b27668385641b5542d654247877411e955d8
Gabi <gabinelepcu29@gmail.com>
2024-08-12 14:27:22 +0300

    update

--- src/pages/Proiecte.jsx
+++ src/pages/Proiecte.jsx
@@ -20,7 +20,7 @@ function UploadProject() {
 
     async function fetchUserDetails() {
       try {
-        const response = await fetch('http://localhost:3000/api/user/me', {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/me`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
