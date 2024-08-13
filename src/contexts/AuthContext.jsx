99560030a02bc2dc6802d8c96901537f25c816d2
Gabi <gabinelepcu29@gmail.com>
2024-08-13 11:26:08 +0300

    update auth

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -8,9 +8,10 @@ const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(null);
     const [userData, setUserData] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const storedToken = Cookies.get('accessToken');
     console.log(import.meta.env)
     useEffect(() => {
-        const storedToken = Cookies.get('accessToken');
+        
         if (storedToken) {
             setToken(storedToken);
             fetchUserData(storedToken);
