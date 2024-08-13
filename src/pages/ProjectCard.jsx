da373d50a957633dac1dfcd5d21283439ddf80f5
Gabi <gabinelepcu29@gmail.com>
2024-08-13 14:28:14 +0300

    update comms

--- src/pages/ProjectCard.jsx
+++ src/pages/ProjectCard.jsx
@@ -4,7 +4,7 @@ import '../styles/ProjectCard.css'; // Adjust path as necessary
 
 const ProjectCard = ({ id, title, description, imageUrl, author, difficulty, type }) => {
   return (
-    <Link to={`/files/${id}`} className="course-card-link">
+    <Link to={`/project/${id}`} className="course-card-link">
       <div className="course-card">
         {imageUrl && <img src={imageUrl} alt={title} style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />}
         <h3>{title}</h3>
